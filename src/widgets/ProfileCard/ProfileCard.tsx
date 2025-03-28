0d9d8f7779d150ce3f488067921557e546b55fbe
gyDex <gyrexcd2@gmail.com>
2025-03-28 22:32:27 +0500

    fix layout view

--- src/widgets/ProfileCard/ProfileCard.tsx
+++ src/widgets/ProfileCard/ProfileCard.tsx
@@ -1,33 +1,18 @@
-'use client'
-import { PlayVideo } from "@/shared/PlayVideo";
 import Image from "next/image";
-import ReactPlayer from "react-player";
-import styles from './ProfileCard.module.scss'
 
 export const ProfileCard = () => {
     return (
-        <>   
-            <div className={`h-[351px] w-[214px] ${styles.player} overflow-hidden rounded-[10px]`} suppressHydrationWarning >
-                <ReactPlayer style={{
-                    width: '100%',
-                    height: '100%'
-                }}
-                
-                playIcon={
-                    <div className="w-full h-full absolute flex justify-center items-center">
-                    <PlayVideo />
-                    </div>
-                } 
-                light={
+        <>
+            <div className="h-[351px] w-[214px] relative overflow-hidden rounded-[10px] flex justify-center">
+                <Image src={'/images/person.png'} alt="person" className="absolute h-[351px] w-[214px] object-cover " height={600} width={214} />
+            
+                <div className="relative z-20 font-normal text-[16px] w-[95%] h-[42px] bg-[#116466] flex justify-center items-center mt-[10px] rounded-[10px] text-[white]">
+                    Подходит больше всего
+                </div>
 
-                        <Image src={'/images/person.png'} alt="person" 
-                        className="h-[351px] w-[214px] object-cover " height={351} width={214} />
-
-
-                }
-                url='https://www.youtube.com/watch?v=LXb3EKWsInQ' 
-                playing />
-                {/* <Image src={'/images/person.png'} alt="person" className="h-[351px] w-[214px] object-cover " height={600} width={214} /> */}
+                <div className="absolute h-full w-full flex justify-center">
+                    <Image src={'/card/PlayVideo.svg'} alt="PlayVideo" height={42} width={42} />
+                </div>
             </div>
         </>
     );
